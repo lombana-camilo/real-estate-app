bf1ea0fa28c59be954caba90f87c36db5af287d6
lombana-camilo <lombana.camilo@hotmail.com>
2022-07-01 12:18:16 -0500

    db settings for heroku deploy

--- server/src/db.js
+++ server/src/db.js
@@ -5,9 +5,40 @@ import ModelUser from "./models/User.js"
 
 const { DB_USER, DB_HOST, DB_NAME, DB_PASSWORD } = process.env;
 
-const db = new Sequelize( `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,{
-   logging:false
-});
+//Settings of sequelize for Heroku Deploy
+let db =
+  process.env.NODE_ENV === "production"
+    ? new Sequelize({
+        database: DB_NAME,
+        dialect: "postgres",
+        host: DB_HOST,
+        port: 5432,
+        username: DB_USER,
+        password: DB_PASSWORD,
+        pool: {
+          max: 3,
+          min: 1,
+          idle: 10000,
+        },
+        dialectOptions: {
+          ssl: {
+            require: true,
+            // Ref.: https://github.com/brianc/node-postgres/issues/2009
+            rejectUnauthorized: false,
+          },
+          keepAlive: true,
+        },
+        ssl: true,
+      })
+    : new Sequelize(
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/development`,
+        { logging: false, native: false }
+      );
+
+// const db = new Sequelize( `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,{
+//    logging:false
+// });
+
 //Inyecting Sequelize connection to models
 ModelProperty(db)
 ModelUser(db)
