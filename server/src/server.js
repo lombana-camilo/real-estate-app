b13b1276cc668c0cd4194dd92593e7c09ceef51c
lombana-camilo <lombana.camilo@hotmail.com>
2022-08-22 00:06:51 -0500

    Cors middleware

--- server/src/server.js
+++ server/src/server.js
@@ -3,14 +3,22 @@ import express from "express";
 import 'dotenv/config'
 import routes from "./routes/index.js";
 import cors from "cors";
-
-
 const server = express()
 
+const allowCORS = function(req, res, next) {
+   var origin = req.get('origin');
+   res.header("Access-Control-Allow-Origin", origin);
+   res.header('Access-Control-Allow-Credentials', 'true');
+   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+   next();
+};
+
+server.use(allowCORS)
 server.use(express.urlencoded({extended:true}))
 server.use(express.json())
-//Origin and credentials allow to send cookies
-server.use(cors({origin:true,credentials:true}))
+// server.use(cors({origin:"https://real-estate-app-kappa-three.vercel.app/",credentials:true}))
+
 server.use(cookieParser())
 
 server.use("/",routes)
